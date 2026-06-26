bcc6304f9d3765de98c9e3313a0beda8ab987fd0
learnodishahelp-cmd <learnodisha.help@gmail.com>
2026-06-26 18:57:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let name = document.getElementById("name").value;
 let mobile = document.getElementById("mobile").value;
 
 
-let response = await fetch("https://cloud.activepieces.com/api/v1/webhooks/A3fmDh7ifefHXjW4Xc2Qx", {
+let response = await fetch("https://cloud.activepieces.com/api/v1/webhooks/A3fmDh7ifefHXjW4Xc2Qx/sync", {
 
 method:"POST",
 
