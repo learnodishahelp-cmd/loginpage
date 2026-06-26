d270c34e9e9dae753022f8c92e61499f878057d8
learnodishahelp-cmd <learnodisha.help@gmail.com>
2026-06-26 18:44:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-document.querySelector("form").addEventListener("submit", async (e)=>{
-  e.preventDefault();
+const form = document.getElementById("studentForm");
 
-  const name = document.querySelector("#name").value;
-  const mobile = document.querySelector("#mobile").value;
+form.addEventListener("submit", async function(e){
 
-  try {
+e.preventDefault();
 
-    const response = await fetch("https://cloud.activepieces.com/api/v1/webhooks/A3fmDh7ifefHXjW4Xc2Qx", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: name,
-        mobile: mobile
-      })
-    });
+let name = document.getElementById("name").value;
+let mobile = document.getElementById("mobile").value;
 
-    const result = await response.text();
 
-    if(response.ok){
-      alert("Form submitted successfully");
-    }
-    else{
-      alert("Server error: " + result);
-    }
+let response = await fetch("https://cloud.activepieces.com/api/v1/webhooks/A3fmDh7ifefHXjW4Xc2Qx", {
 
-  } catch(error){
-    alert("Form submitting error");
-    console.log(error);
-  }
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+name:name,
+mobile:mobile
+})
+
+});
+
+
+if(response.ok){
+alert("Submitted successfully");
+}
+else{
+alert("Error submitting form");
+}
 
 });
